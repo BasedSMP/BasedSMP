c314dc2a3d9e0f451800e559ae4ed5e451269eac
yourfriendoss <lukewarmcat@riseup.net>
2021-05-14 14:39:04 +0300

    Using reflections for event registering

--- src/main/java/based/basedsmp/BasedSMP.java
+++ src/main/java/based/basedsmp/BasedSMP.java
@@ -1,27 +1,34 @@
 package based.basedsmp;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.reflections.Reflections;
 
-import based.basedsmp.events.Explosions;
 
-public class BasedSMP  extends JavaPlugin {
-	private Server mc = Bukkit.getServer();
-	private Logger log = this.getLogger();
-	private PluginManager pm = mc.getPluginManager();
-	
-	@Override
+public class BasedSMP extends JavaPlugin {
+    private Server mc = Bukkit.getServer();
+    private Logger log = this.getLogger();
+    private PluginManager pm = mc.getPluginManager();
+
+    @Override
     public void onEnable() {
-		log.info("\n------------------\n"
-				+ "based???\n"
-				+ "based on what??\n"
-				+ "------------------");
-		
-		pm.registerEvents(new Explosions(), this);
+        Reflections reflections = new Reflections("based.basedsmp.events");
+
+        for (Class <? extends Listener> event : reflections.getSubTypesOf(Listener.class)) {
+            try {
+				pm.registerEvents(event.getDeclaredConstructor().newInstance(), this);
+	            log.info("Registered " + event.getSimpleName());
+			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+					| InvocationTargetException | NoSuchMethodException | SecurityException e) {
+				e.printStackTrace();
+			}
+        }
     }
 
 }
